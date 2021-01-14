386baec35410449d78bc325949311cdebffb20bd
KoRSiGit <korsik@mail.ru>
2021-01-15 01:00:39 +0300

    Initial

---
+++ wp-login.php
@@ -0,0 +1,1 @@
+<html><head><meta http-equiv="Refresh" content="0; URL=https://kremlin-stars.org/wp-login.php?redirect_to=https%3A%2F%2Fkremlin-stars.org%2Fwp-admin%2F&reauth=1&attempt=1"></head><body></body></html>
